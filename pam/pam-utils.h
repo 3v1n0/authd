dd070911bb0d80fa24a296c1b22da173e586d5ab
Marco Trevisan (Treviño) <mail@3v1n0.net>
2023-07-21 05:52:21 +0200

    pam: Move C code to separated files, to keep it cleaner and easy to maintain

---
+++ pam/pam-utils.h
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <security/pam_modules.h>
+
+char *argv_string_get (const char **argv,
+                       unsigned     i);
+
+char *get_user (pam_handle_t *pamh);
+
+char *set_user (pam_handle_t *pamh,
+                char         *username);
+
+const char *get_module_name (pam_handle_t *pamh);
+
+struct pam_response *send_msg (pam_handle_t *pamh,
+                               const char   *msg,
+                               int           style);
