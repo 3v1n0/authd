2d577df98cf48cda437b30b54baee70f156911c6
Marco Trevisan (Treviño) <mail@3v1n0.net>
2023-09-05 19:59:55 +0200

    pam: Add initial GDM JSON protocol definition
    We use JSON to communicate with GDM since its UI, GNOME Shell is in
    JavaScript and so can handle those messages natively pretty well.
    
    Here we define the primitives of this protocol and their conversion back
    and forth from go structures and types, ensuring the expectations are
    met.

---
+++ pam/gdm/protocol.go
@@ -0,0 +1,374 @@
+// Package gdm is the package for the GDM pam module handing.
+package gdm
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"reflect"
+	"slices"
+)
+
+const (
+	// ProtoVersion is the version of the JSON protocol.
+	ProtoVersion = int(1)
+)
+
+// DataType represents the type of a communication event.
+type DataType int
+
+// Object is the type for any generic object data value.
+type Object = map[string]any
+
+// Data is the serializable structure that can be passed to Gdm and that
+// we expect gdm to return us.
+type Data struct {
+	Type             DataType    `json:"type"`
+	HelloData        *HelloData  `json:"helloData,omitempty"`
+	RequestType      RequestType `json:"requestType,omitempty"`
+	RequestData      Object      `json:"requestData,omitempty"`
+	ResponseData     []any       `json:"responseData,omitempty"`
+	PollResponseData []Data      `json:"pollResponseData,omitempty"`
+	EventType        EventType   `json:"eventType,omitempty"`
+	EventData        Object      `json:"eventData,omitempty"`
+}
+
+// NewDataFromJSON unmarshals data from json bytes.
+func NewDataFromJSON(bytes []byte) (*Data, error) {
+	var gdmData Data
+	if err := json.Unmarshal(bytes, &gdmData); err != nil {
+		return nil, err
+	}
+
+	if err := gdmData.Check(); err != nil {
+		return nil, err
+	}
+
+	return &gdmData, nil
+}
+
+func (d Data) checkMembers(acceptedMembers []string) error {
+	val := reflect.ValueOf(d)
+	typ := val.Type()
+	acceptedMembers = append(acceptedMembers, "Type")
+
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Field(i)
+		fieldType := typ.Field(i)
+
+		if slices.Contains(acceptedMembers, fieldType.Name) {
+			continue
+		}
+
+		if !field.IsZero() {
+			return fmt.Errorf("field %v should not be defined", fieldType.Name)
+		}
+	}
+
+	return nil
+}
+
+// Check allows to check the sanity of a data value.
+func (d Data) Check() error {
+	switch d.Type {
+	case UnknownType:
+		return fmt.Errorf("unexpected type %v", d.Type.String())
+
+	case Hello:
+		if err := d.checkMembers([]string{"HelloData"}); err != nil {
+			return err
+		}
+
+	case Event:
+		if d.EventType == UnknownEvent {
+			return errors.New("missing event type")
+		}
+		if d.EventType >= lastEventType {
+			return fmt.Errorf("unexpected event type %v", d.EventType)
+		}
+		if d.EventData == nil {
+			return fmt.Errorf("missing event data")
+		}
+		if err := d.checkMembers([]string{"EventType", "EventData"}); err != nil {
+			return err
+		}
+
+	case EventAck:
+		if err := d.checkMembers([]string{}); err != nil {
+			return err
+		}
+
+	case Request:
+		if d.RequestType == UnknownRequest {
+			return errors.New("missing request type")
+		}
+		if d.RequestType >= lastRequestType {
+			return fmt.Errorf("unexpected request type %v", d.RequestType)
+		}
+		if err := d.checkMembers([]string{"RequestType", "RequestData"}); err != nil {
+			return err
+		}
+
+	case Response:
+		if d.ResponseData == nil {
+			return errors.New("missing response data")
+		}
+		if err := d.checkMembers([]string{"ResponseData"}); err != nil {
+			return err
+		}
+
+	case Poll:
+		if err := d.checkMembers([]string{}); err != nil {
+			return err
+		}
+
+	case PollResponse:
+		if err := d.checkMembers([]string{"PollResponseData"}); err != nil {
+			return err
+		}
+		for i, response := range d.PollResponseData {
+			switch response.Type {
+			case Event:
+			default:
+				return fmt.Errorf("poll response data member %v unsupported type: %v",
+					i, response.Type.String())
+			}
+			if err := response.Check(); err != nil {
+				return fmt.Errorf("poll response data member %v invalid: %v", i, err)
+			}
+		}
+
+	default:
+		return fmt.Errorf("unhandled type %v", d.Type)
+	}
+
+	return nil
+}
+
+// JSON returns the data object serialized as JSON bytes.
+func (d *Data) JSON() ([]byte, error) {
+	bytes, err := json.Marshal(d)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = d.Check(); err != nil {
+		return nil, err
+	}
+
+	return bytes, err
+}
+
+const (
+	// UnknownType is an unknown DataType.
+	UnknownType DataType = iota
+	// Hello is an hello DataType.
+	Hello
+	// Event is an event DataType.
+	Event
+	// EventAck is an event DataType.
+	EventAck
+	// Request is a request DataType.
+	Request
+	// Response is a response DataType.
+	Response
+	// Poll is a poll DataType.
+	Poll
+	// PollResponse is a poll response DataType.
+	PollResponse
+
+	// lastDataType is the last DataType value, so keep it as such.
+	lastDataType
+)
+
+func (t DataType) String() string {
+	switch t {
+	case Hello:
+		return "hello"
+	case Event:
+		return "event"
+	case EventAck:
+		return "eventAck"
+	case Request:
+		return "request"
+	case Response:
+		return "response"
+	case Poll:
+		return "poll"
+	case PollResponse:
+		return "pollResponse"
+	default:
+		return "unknownType"
+	}
+}
+
+// UnmarshalJSON unmarshals a DataType from json bytes.
+func (t *DataType) UnmarshalJSON(b []byte) error {
+	var s string
+	if err := json.Unmarshal(b, &s); err != nil {
+		return err
+	}
+
+	for i := DataType(0); i < lastDataType; i++ {
+		if i.String() == s {
+			*t = i
+			return nil
+		}
+	}
+
+	*t = UnknownType
+	return nil
+}
+
+// MarshalJSON marshals DataType to JSON bytes.
+func (t DataType) MarshalJSON() ([]byte, error) {
+	return json.Marshal(t.String())
+}
+
+// HelloData represents the struct type for an Hello message.
+type HelloData struct {
+	Version int `json:"version"`
+}
+
+// EventType defines the types for the events that we support.
+type EventType int
+
+const (
+	// UnknownEvent is an unknown EventType.
+	UnknownEvent EventType = iota
+	// UserSelected is an user selected EventType.
+	UserSelected
+	// BrokersReceived is a broker received EventType.
+	BrokersReceived
+	// BrokerSelected is a broker selected EventType.
+	BrokerSelected
+	// AuthModesReceived is an auth modes received EventType.
+	AuthModesReceived
+	// AuthModeSelected is an auth mode selected EventType.
+	AuthModeSelected
+	// ReselectAuthMode is an reselect auth mode selected EventType.
+	ReselectAuthMode
+	// AuthEvent is an auth event EventType.
+	AuthEvent
+	// UILayoutReceived is an UI Layout Received EventType.
+	UILayoutReceived
+	// StartAuthentication is a start authentication EventType.
+	StartAuthentication
+	// IsAuthenticatedRequested is an authentication request EventType.
+	IsAuthenticatedRequested
+	// StageChanged is stage changed EventType.
+	StageChanged
+
+	// lastEventType is the last EventType value, so keep it as such.
+	lastEventType
+)
+
+func (e EventType) String() string {
+	switch e {
+	case UserSelected:
+		return "userSelected"
+	case BrokerSelected:
+		return "brokerSelected"
+	case AuthModesReceived:
+		return "authModesReceived"
+	case AuthModeSelected:
+		return "authModeSelected"
+	case ReselectAuthMode:
+		return "reselectAuthMode"
+	case AuthEvent:
+		return "authEvent"
+	case BrokersReceived:
+		return "brokersReceived"
+	case UILayoutReceived:
+		return "uiLayoutReceived"
+	case StartAuthentication:
+		return "startAuthentication"
+	case IsAuthenticatedRequested:
+		return "isAuthenticatedRequested"
+	case StageChanged:
+		return "stageChanged"
+	default:
+		return "unknownEvent"
+	}
+}
+
+// UnmarshalJSON unmarshals a EventType from json bytes.
+func (e *EventType) UnmarshalJSON(b []byte) error {
+	var s string
+	if err := json.Unmarshal(b, &s); err != nil {
+		return err
+	}
+
+	for i := EventType(0); i < lastEventType; i++ {
+		if i.String() == s {
+			*e = i
+			return nil
+		}
+	}
+
+	*e = UnknownEvent
+
+	return nil
+}
+
+// MarshalJSON marshals EventType to JSON bytes.
+func (e EventType) MarshalJSON() ([]byte, error) {
+	return json.Marshal(e.String())
+}
+
+// RequestType represents the the supported requests.
+type RequestType int
+
+const (
+	// UnknownRequest is an unknown request RequestType.
+	UnknownRequest RequestType = iota
+	// UpdateBrokersList is an update brokers list RequestType.
+	UpdateBrokersList
+	// ComposeAuthenticationView is an compose authentication view RequestType.
+	ComposeAuthenticationView
+	// UILayoutCapabilities is an ui layout capabilities RequestType.
+	UILayoutCapabilities
+	// ChangeStage is a change stage RequestType.
+	ChangeStage
+
+	// lastRequestType is the last RequestType value, so keep it as such.
+	lastRequestType
+)
+
+// UnmarshalJSON unmarshals a RequestType from json bytes.
+func (r *RequestType) UnmarshalJSON(b []byte) error {
+	var s string
+	if err := json.Unmarshal(b, &s); err != nil {
+		return err
+	}
+
+	for i := RequestType(0); i < lastRequestType; i++ {
+		if i.String() == s {
+			*r = i
+			return nil
+		}
+	}
+
+	*r = UnknownRequest
+	return nil
+}
+
+func (r RequestType) String() string {
+	switch r {
+	case UpdateBrokersList:
+		return "updateBrokersList"
+	case ComposeAuthenticationView:
+		return "composeAuthenticationView"
+	case UILayoutCapabilities:
+		return "uiLayoutCapabilities"
+	case ChangeStage:
+		return "changeStage"
+	default:
+		return "unknownRequest"
+	}
+}
+
+// MarshalJSON marshals RequestType to JSON bytes.
+func (r RequestType) MarshalJSON() ([]byte, error) {
+	return json.Marshal(r.String())
+}
---
+++ pam/gdm/protocol_test.go
@@ -0,0 +1,535 @@
+package gdm
+
+import (
+	"encoding/json"
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestGdmStructsMarshal(t *testing.T) {
+	t.Parallel()
+	tests := map[string]struct {
+		gdmData            Data
+		ignoreReconversion bool
+
+		wantJSON  string
+		wantError string
+	}{
+		"Hello packet": {
+			gdmData:  Data{Type: Hello},
+			wantJSON: `{"type":"hello"}`,
+		},
+
+		"Hello packet with data": {
+			gdmData:  Data{Type: Hello, HelloData: &HelloData{Version: 55}},
+			wantJSON: `{"type":"hello","helloData":{"version":55}}`,
+		},
+
+		"Event packet": {
+			gdmData: Data{
+				Type: Event, EventType: BrokerSelected,
+				EventData: Object{"name": "foo"},
+			},
+			wantJSON: `{"type":"event","eventType":"brokerSelected",` +
+				`"eventData":{"name":"foo"}}`,
+		},
+
+		"Event packet with capital name": {
+			gdmData: Data{
+				Type: Event, EventType: UILayoutReceived,
+				EventData: Object{"name": "bar"},
+			},
+			wantJSON: `{"type":"event","eventType":"uiLayoutReceived",` +
+				`"eventData":{"name":"bar"}}`,
+		},
+
+		"Event packet with unknown type": {
+			gdmData:   Data{Type: Event, EventType: UnknownEvent},
+			wantError: "missing event type",
+		},
+
+		"Event ack packet": {
+			gdmData:  Data{Type: EventAck},
+			wantJSON: `{"type":"eventAck"}`,
+		},
+
+		"Request packet": {
+			gdmData: Data{
+				Type: Request, RequestType: UILayoutCapabilities,
+				RequestData: Object{"name": "foo"},
+			},
+			wantJSON: `{"type":"request","requestType":"uiLayoutCapabilities",` +
+				`"requestData":{"name":"foo"}}`,
+		},
+
+		"Request packet with missing data": {
+			gdmData:  Data{Type: Request, RequestType: UpdateBrokersList},
+			wantJSON: `{"type":"request","requestType":"updateBrokersList"}`,
+		},
+
+		"Response packet": {
+			gdmData: Data{
+				Type:         Response,
+				ResponseData: []any{Object{"name": "foo"}, Object{"name": "bar"}},
+			},
+			wantJSON: `{"type":"response","responseData":[{"name":"foo"},{"name":"bar"}]}`,
+		},
+
+		"Response packet with empty data": {
+			gdmData: Data{
+				Type:         Response,
+				ResponseData: []any{},
+			},
+			wantJSON:           `{"type":"response"}`,
+			ignoreReconversion: true,
+		},
+
+		"Response packet with mixed data": {
+			gdmData: Data{
+				Type: Response,
+				ResponseData: []any{
+					Object{"name": "foo"}, true, "string", float64(12345), 0.55,
+				},
+			},
+			wantJSON: `{"type":"response","responseData":[{"name":"foo"},true,"string",12345,0.55]}`,
+		},
+
+		"Poll packet": {
+			gdmData:  Data{Type: Poll},
+			wantJSON: `{"type":"poll"}`,
+		},
+
+		"PollResponse packet": {
+			gdmData: Data{
+				Type: PollResponse,
+				PollResponseData: []Data{
+					{
+						Type:      Event,
+						EventType: BrokerSelected,
+						EventData: Object{"foo": "bar"},
+					},
+				},
+			},
+			wantJSON: `{"type":"pollResponse","pollResponseData":` +
+				`[{"type":"event","eventType":"brokerSelected","eventData":{"foo":"bar"}}]}`,
+		},
+
+		"PollResponse packet with empty data": {
+			gdmData: Data{
+				Type:             PollResponse,
+				PollResponseData: []Data{},
+			},
+			wantJSON:           `{"type":"pollResponse"}`,
+			ignoreReconversion: true,
+		},
+
+		// Error cases
+		"Error empty packet": {
+			wantError: "unexpected type unknownType",
+		},
+
+		"Error if packet has invalid type": {
+			gdmData:   Data{Type: lastDataType},
+			wantError: "unhandled type unknownType",
+		},
+
+		"Error hello packet with unexpected data": {
+			gdmData:   Data{Type: Hello, RequestData: Object{"foo": 1}},
+			wantError: "field RequestData should not be defined",
+		},
+
+		"Error event packet with invalid type": {
+			gdmData:   Data{Type: Event, EventType: lastEventType},
+			wantError: "unexpected event type",
+		},
+
+		"Error event packet with missing data": {
+			gdmData:   Data{Type: Event, EventType: UserSelected},
+			wantError: "missing event data",
+		},
+
+		"Error event packet with unexpected data": {
+			gdmData: Data{
+				Type: Event, EventType: BrokerSelected,
+				EventData:   Object{"name": "foo"},
+				RequestData: Object{"foo": "bar"},
+			},
+			wantError: "field RequestData should not be defined",
+		},
+
+		"Error event ack packet with unexpected data": {
+			gdmData:   Data{Type: EventAck, EventData: Object{}},
+			wantError: "field EventData should not be defined",
+		},
+
+		"Error request packet with unknown type": {
+			gdmData:   Data{Type: Request, RequestType: UnknownRequest},
+			wantError: "missing request type",
+		},
+
+		"Error request packet with invalid type": {
+			gdmData:   Data{Type: Request, RequestType: lastRequestType},
+			wantError: "unexpected request type",
+		},
+
+		"Error request packet with unexpected data": {
+			gdmData: Data{
+				Type: Request, RequestType: UILayoutCapabilities,
+				RequestData: Object{"name": "foo"},
+				EventData:   Object{"foo": "bar"},
+			},
+			wantError: "field EventData should not be defined",
+		},
+
+		"Error response packet with missing data": {
+			gdmData:   Data{Type: Response},
+			wantError: "missing response data",
+		},
+
+		"Error response packet with unexpected data": {
+			gdmData: Data{
+				Type:         Response,
+				ResponseData: []any{Object{"name": "foo"}},
+				EventData:    Object{"foo": "bar"},
+			},
+			wantError: "field EventData should not be defined",
+		},
+
+		"Error poll packet with unexpected data": {
+			gdmData:   Data{Type: Poll, RequestData: Object{"foo": 1}},
+			wantError: "field RequestData should not be defined",
+		},
+
+		"Error pollResponse packet with missing event type": {
+			gdmData: Data{
+				Type: PollResponse,
+				PollResponseData: []Data{
+					{
+						Type:      Event,
+						EventType: BrokerSelected,
+						EventData: Object{"foo": "bar"},
+					},
+					{
+						Type:      Event,
+						EventData: Object{"broken": "yes"},
+					},
+				},
+			},
+			wantError: "poll response data member 1 invalid: missing event type",
+		},
+
+		"Error pollResponse packet with unsupported data type": {
+			gdmData: Data{
+				Type: PollResponse,
+				PollResponseData: []Data{
+					{
+						Type:      Event,
+						EventType: BrokerSelected,
+						EventData: Object{"foo": "bar"},
+					},
+					{
+						Type: Poll,
+					},
+				},
+			},
+			wantError: "poll response data member 1 unsupported type: poll",
+		},
+
+		"Error pollResponse packet with unexpected data": {
+			gdmData: Data{
+				Type:             PollResponse,
+				PollResponseData: []Data{},
+				EventData:        Object{"foo": "bar"},
+			},
+			wantError: "field EventData should not be defined",
+		},
+	}
+
+	for name, tc := range tests {
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			bytes, err := tc.gdmData.JSON()
+			if tc.wantError != "" {
+				require.ErrorContains(t, err, tc.wantError)
+				return
+			}
+
+			require.NoError(t, err)
+			require.Equal(t, tc.wantJSON, string(bytes))
+
+			if !tc.ignoreReconversion {
+				gdmData, err := NewDataFromJSON(bytes)
+				require.NoError(t, err)
+				require.Equal(t, tc.gdmData, *gdmData)
+			}
+		})
+	}
+}
+
+func TestGdmStructsUnMarshal(t *testing.T) {
+	t.Parallel()
+	tests := map[string]struct {
+		JSON               string
+		ignoreReconversion bool
+
+		wantData  Data
+		wantError string
+	}{
+		"hello packet": {
+			JSON:     `{"type":"hello"}`,
+			wantData: Data{Type: Hello},
+		},
+
+		"Hello packet with data": {
+			JSON:     `{"type":"hello","helloData":{"version":55}}`,
+			wantData: Data{Type: Hello, HelloData: &HelloData{Version: 55}},
+		},
+
+		"Event packet": {
+			JSON: `{"type":"event","eventType":"brokerSelected",` +
+				`"eventData":{"name":"foo"}}`,
+			wantData: Data{
+				Type: Event, EventType: BrokerSelected,
+				EventData: Object{"name": "foo"},
+			},
+		},
+
+		"Event packet with capital name": {
+			JSON: `{"type":"event","eventType":"uiLayoutReceived",` +
+				`"eventData":{"name":"bar"}}`,
+			wantData: Data{
+				Type: Event, EventType: UILayoutReceived,
+				EventData: Object{"name": "bar"},
+			},
+		},
+
+		"Event ack packet": {
+			JSON:     `{"type":"eventAck"}`,
+			wantData: Data{Type: EventAck},
+		},
+
+		"Request packet": {
+			JSON: `{"type":"request","requestType":"uiLayoutCapabilities",` +
+				`"requestData":{"name":"foo"}}`,
+			wantData: Data{
+				Type: Request, RequestType: UILayoutCapabilities,
+				RequestData: Object{"name": "foo"},
+			},
+		},
+
+		"Request packet with missing data": {
+			JSON:     `{"type":"request","requestType":"updateBrokersList"}`,
+			wantData: Data{Type: Request, RequestType: UpdateBrokersList},
+		},
+
+		"Response packet": {
+			JSON: `{"type":"response","responseData":[{"name":"foo"},{"name":"bar"}]}`,
+			wantData: Data{
+				Type:         Response,
+				ResponseData: []any{Object{"name": "foo"}, Object{"name": "bar"}},
+			},
+		},
+
+		"Response packet with empty data": {
+			JSON: `{"type":"response","responseData":[]}`,
+			wantData: Data{
+				Type:         Response,
+				ResponseData: []any{},
+			},
+			ignoreReconversion: true,
+		},
+
+		"Response packet with mixed data": {
+			JSON: `{"type":"response","responseData":[{"name":"foo"},true,"string",12345,0.55]}`,
+			wantData: Data{
+				Type: Response,
+				ResponseData: []any{
+					Object{"name": "foo"}, true, "string", float64(12345), 0.55,
+				},
+			},
+		},
+
+		"Poll packet": {
+			JSON:     `{"type":"poll"}`,
+			wantData: Data{Type: Poll},
+		},
+
+		"PollResponse packet": {
+			JSON: `{"type":"pollResponse","pollResponseData":` +
+				`[{"type":"event","eventType":"brokerSelected","eventData":{"foo":"bar"}}]}`,
+			wantData: Data{
+				Type: PollResponse,
+				PollResponseData: []Data{
+					{
+						Type:      Event,
+						EventType: BrokerSelected,
+						EventData: Object{"foo": "bar"},
+					},
+				},
+			},
+		},
+
+		"PollResponse packet with empty data": {
+			JSON: `{"type":"pollResponse"}`,
+			wantData: Data{
+				Type:             PollResponse,
+				PollResponseData: nil,
+			},
+			ignoreReconversion: true,
+		},
+
+		// Error cases
+		"Error empty packet ": {
+			wantError: "unexpected end of JSON input",
+		},
+
+		"Error empty packet object": {
+			JSON:      `{}`,
+			wantError: "unexpected type unknownType",
+		},
+
+		"Error packet with invalid type": {
+			JSON:      `{"type":"invalidType"}`,
+			wantError: "unexpected type unknownType",
+		},
+
+		"Error packet with invalid value type": {
+			JSON: `{"type":[]}`,
+			wantError: (&json.UnmarshalTypeError{
+				Value:  "array",
+				Struct: "Data",
+				Field:  "type",
+				Type:   reflect.TypeOf(""),
+			}).Error(),
+		},
+
+		"Error hello packet with unexpected data": {
+			JSON:      `{"type":"hello","requestType":"updateBrokersList"}`,
+			wantError: "field RequestType should not be defined",
+		},
+
+		"Error event packet with missing type": {
+			JSON:      `{"type":"event","eventData":{"name":"foo"}}`,
+			wantError: "missing event type",
+		},
+
+		"Error event packet with unknown type": {
+			JSON:      `{"type":"event","eventData":{"name":"foo"},"eventType":"someType"}`,
+			wantError: "missing event type",
+		},
+
+		"Error event packet with invalid value type": {
+			JSON: `{"type":"event","eventData":{"name":"foo"},"eventType":{}}`,
+			wantError: (&json.UnmarshalTypeError{
+				Value:  "object",
+				Struct: "Data",
+				Field:  "eventType",
+				Type:   reflect.TypeOf(""),
+			}).Error(),
+		},
+
+		"Error event packet with missing data": {
+			JSON:      `{"type":"event","eventType":"brokerSelected"}`,
+			wantError: "missing event data",
+		},
+
+		"Error event packet with unexpected data": {
+			JSON: `{"type":"event","eventType":"brokerSelected",` +
+				`"eventData":{"name":"foo"},"requestType":"updateBrokersList"}`,
+			wantError: "field RequestType should not be defined",
+		},
+
+		"Error event ack packet with unexpected member": {
+			JSON:      `{"type":"eventAck","eventData":{"name":"foo"}}`,
+			wantError: "field EventData should not be defined",
+		},
+
+		"Error request packet with missing type": {
+			JSON:      `{"type":"request","requestData":{"name":"foo"}}`,
+			wantError: "missing request type",
+		},
+
+		"Error request packet with unknown type": {
+			JSON: `{"type":"request","requestType":true,` +
+				`"requestData":{"name":"foo"}}`,
+			wantError: (&json.UnmarshalTypeError{
+				Value:  "bool",
+				Struct: "Data",
+				Field:  "requestType",
+				Type:   reflect.TypeOf(""),
+			}).Error(),
+		},
+
+		"Error request packet with unknown value type": {
+			JSON: `{"type":"request","requestType":"someUnknownRequest",` +
+				`"requestData":{"name":"foo"}}`,
+			wantError: "missing request type",
+		},
+
+		"Error request packet with unexpected data": {
+			JSON: `{"type":"request","requestType":"uiLayoutCapabilities",` +
+				`"requestData":{"name":"foo"}, "eventType":"brokerSelected"}`,
+			wantError: "field EventType should not be defined",
+		},
+
+		"Error response packet with missing data": {
+			JSON:      `{"type":"response"}`,
+			wantError: "missing response data",
+		},
+
+		"Error response packet with unexpected data": {
+			JSON:      `{"type":"response","responseData":[], "eventType":"brokerSelected"}`,
+			wantError: "field EventType should not be defined",
+		},
+
+		"Error poll packet with unexpected data": {
+			JSON:      `{"type":"poll", "responseData": []}`,
+			wantError: "field ResponseData should not be defined",
+		},
+
+		"Error pollResponse packet with missing event type": {
+			JSON: `{"type":"pollResponse","pollResponseData":` +
+				`[{"type":"event","eventType":"brokerSelected","eventData":{"foo":"bar"}},` +
+				`{"type":"event","eventData":{"foo":"bar"}}]}`,
+			wantError: "poll response data member 1 invalid: missing event type",
+		},
+
+		"Error pollResponse packet with unsupported data type": {
+			JSON: `{"type":"pollResponse","pollResponseData":` +
+				`[{"type":"event","eventType":"brokerSelected","eventData":{"foo":"bar"}},` +
+				`{"type":"poll"}]}`,
+			wantError: "poll response data member 1 unsupported type: poll",
+		},
+
+		"Error pollResponse packet with unexpected data": {
+			JSON: `{"type":"pollResponse","pollResponseData":` +
+				`[{"type":"event","eventType":"brokerSelected","eventData":{"foo":"bar"}}],` +
+				`"responseData": []}`,
+			wantError: "field ResponseData should not be defined",
+		},
+	}
+
+	for name, tc := range tests {
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			gdmData, err := NewDataFromJSON([]byte(tc.JSON))
+			if tc.wantError != "" {
+				require.ErrorContains(t, err, tc.wantError)
+				return
+			}
+
+			require.NoError(t, err)
+			require.Equal(t, tc.wantData, *gdmData)
+
+			if !tc.ignoreReconversion {
+				json, err := gdmData.JSON()
+				require.NoError(t, err)
+				require.Equal(t, tc.JSON, string(json))
+			}
+		})
+	}
+}
