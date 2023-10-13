d7cad21ee3858038d86234c9b0091c10e6ab1700
Marco Trevisan (Treviño) <mail@3v1n0.net>
2023-10-14 00:59:49 +0200

    pam: Simplify the return code handling by using pam.TransactionError
    Instead of using our own types for return status, let's just implement
    pam.TransactionError to return more complex error values back to the
    user

--- pam/return.go
+++ pam/return.go
@@ -1,64 +1,45 @@
 package main
 
+import (
+	"github.com/msteinert/pam"
+)
+
 // Various signalling return messaging to PAM.
 
-// pamSuccess signals PAM module to return PAM_SUCCESS and Quit tea.Model.
+type pamReturnStatus interface {
+	Status() pam.Status
+	Error() string
+}
+
+// newPamStatus returns a new pamStatus.
+func newPamStatus(status pam.Status, err error) pamReturnStatus {
+	return pam.NewTransactionError(err, status)
+}
+
+// pamStatus signals PAM module to return with provided pam.Success and Quit tea.Model.
 type pamSuccess struct {
+	pam.TransactionError
 	brokerID string
 }
 
-// String returns the string of pamSuccess.
-func (err pamSuccess) String() string {
-	return ""
+// newPamSuccess returns a new pamSuccess.
+func newPamSuccess(brokerID string) pamSuccess {
+	return pamSuccess{
+		pam.NewTransactionError(nil, pam.Success),
+		brokerID,
+	}
 }
 
-// pamIgnore signals PAM module to return PAM_IGNORE and Quit tea.Model.
+// pamIgnore signals PAM module to return pam.Ignore and Quit tea.Model.
 type pamIgnore struct {
+	pam.TransactionError
 	localBrokerID string // Only set for local broker to store it globally.
-	msg           string
 }
 
-// String returns the string of pamIgnore message.
-func (err pamIgnore) String() string {
-	return err.msg
-}
-
-// pamAbort signals PAM module to return PAM_ABORT and Quit tea.Model.
-type pamAbort struct {
-	msg string
-}
-
-// String returns the string of pamAbort message.
-func (err pamAbort) String() string {
-	return err.msg
-}
-
-// pamSystemError signals PAM module to return PAM_SYSTEM_ERROR and Quit tea.Model.
-type pamSystemError struct {
-	msg string
-}
-
-// String returns the string of pamSystemError message.
-func (err pamSystemError) String() string {
-	return err.msg
-}
-
-// pamAuthError signals PAM module to return PAM_AUTH_ERROR and Quit tea.Model.
-type pamAuthError struct {
-	msg string
-}
-
-// String returns the string of pamAuthError message.
-func (err pamAuthError) String() string {
-	return err.msg
-}
-
-// pamAuthInfoUnavailable signals PAM module to return PAM_AUTHINFO_UNAVAIL and Quit tea.Model.
-type pamAuthInfoUnavailable struct {
-	msg string
-}
-
-// String returns the string of pamAuthInfoUnavailable message.
-func (err pamAuthInfoUnavailable) String() string {
-	return err.msg
+// newPamIgnore returns a new pamIgnore.
+func newPamIgnore(localBrokerID string, err error) pamIgnore {
+	return pamIgnore{
+		pam.NewTransactionError(err, pam.Success),
+		localBrokerID,
+	}
 }
