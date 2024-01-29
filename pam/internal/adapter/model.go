102cb67446c8b0d309a15988fe08a462ff61ba0f
Marco Trevisan (Treviño) <mail@3v1n0.net>
2024-01-29 10:01:51 +0100

    pam/adapter/model: Only store the exit status when received
    And prevent overriding it, in case multiple concurrent events are
    causing an error, we should just stick with the first one we got

--- pam/internal/adapter/model.go
+++ pam/internal/adapter/model.go
@@ -97,7 +97,6 @@ type ChangeStage struct {
 
 // Init initializes the main model orchestrator.
 func (m *UIModel) Init() tea.Cmd {
-	m.exitStatus = pamError{status: pam.ErrSystem, msg: "model did not return anything"}
 	m.userSelectionModel = newUserSelectionModel(m.PamMTx, m.ClientType)
 	var cmds []tea.Cmd
 	cmds = append(cmds, m.userSelectionModel.Init())
@@ -150,6 +149,10 @@ func (m *UIModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 	// Exit cases
 	case PamReturnStatus:
+		if m.exitStatus != nil {
+			// Nothing to do, we're already exiting...
+			return m, nil
+		}
 		m.exitStatus = msg
 		return m, m.quit()
 
@@ -340,6 +343,9 @@ func (m *UIModel) changeStage(s pam_proto.Stage) tea.Cmd {
 
 // ExitStatus exposes the [PamReturnStatus] externally.
 func (m *UIModel) ExitStatus() PamReturnStatus {
+	if m.exitStatus == nil {
+		return pamError{status: pam.ErrSystem, msg: "model did not return anything"}
+	}
 	return m.exitStatus
 }
 
