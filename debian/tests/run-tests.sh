8b82fe437682dcc896072c0d942881d6003546f9
Marco Trevisan (Treviño) <mail@3v1n0.net>
2024-02-23 11:57:41 +0100

    debian/tests: Use a script to launch autopkgtests
    It makes things easier to catch and at the same time we can enforce more
    things such as disable internet access and force using local go
    toolchain

---
+++ debian/tests/run-tests.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -exuo pipefail
+
+export AUTHD_SKIP_EXTERNAL_DEPENDENT_TESTS=1
+export GOPROXY=off
+export GOTOOLCHAIN=local
+
+go generate ./...
+go test -mod=vendor -v ./...
