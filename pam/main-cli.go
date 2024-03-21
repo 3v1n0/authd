731d1ad9e725fcbfa320450a557ff369008eb506
Marco Trevisan (Treviño) <mail@3v1n0.net>
2024-03-21 07:49:42 +0100

    pam: Re-implement the pam cli client as a real PAM application
    Instead of directly calling the client operations, do what PAM does, so:
     - Once the required binaries and libraries are built
     - Setup a service file to use them and with the required arguments
     - Perform the operation going through libpam
    
     This works as expected and can now replace the integration tests that
     are now way closer to reality.
    
     Golden files needed to be updated since some error messages aren't
     shown anymore because they're not exposed as PAM messages but they were
     just go errors that we don't have at this level.
    
     Nothing changes though, in fact the "ignore" errors are now "try again"
     erros for the way we've configured the service file.

--- pam/main-cli.go
+++ pam/main-cli.go
@@ -5,19 +5,73 @@ package main
 import (
 	"errors"
 	"fmt"
+	"log"
 	"os"
+	"os/exec"
 	"path/filepath"
+	"strings"
 
 	"github.com/msteinert/pam/v2"
+	"github.com/ubuntu/authd/internal/testutils"
 	"github.com/ubuntu/authd/pam/internal/pam_test"
 )
 
 // Simulating pam on the CLI for manual testing.
 func main() {
 	logDir := os.Getenv("AUTHD_PAM_CLI_LOG_DIR")
+	execModule := os.Getenv("AUTHD_PAM_EXEC_MODULE")
+	cliPath := os.Getenv("AUTHD_PAM_CLI_PATH")
 
-	module := &pamModule{}
-	mTx := pam_test.NewModuleTransactionDummy(pam.ConversationFunc(
+	tmpDir, err := os.MkdirTemp(os.TempDir(), "pam-cli-tester-")
+	if err != nil {
+		log.Fatalf("Can't create temporary dir: %v", err)
+	}
+	defer os.RemoveAll(tmpDir)
+
+	if _, err := os.Stat(execModule); err != nil {
+		execModule, err = buildExecModule(tmpDir)
+		if err != nil {
+			log.Fatalf("Module build failed: %v", err)
+		}
+	}
+
+	if _, err := os.Stat(cliPath); err != nil {
+		cliPath, err = buildClient(tmpDir)
+		if err != nil {
+			log.Fatalf("Client build failed: %v", err)
+		}
+	}
+
+	defaultArgs := []string{cliPath, "debug=true"}
+	if logDir != "" {
+		logPath := filepath.Join(logDir, "authd-pam-cli.log")
+		defaultArgs = append(defaultArgs, "logfile="+logPath)
+	}
+
+	if coverDir := os.Getenv("GOCOVERDIR"); coverDir != "" {
+		defaultArgs = append(defaultArgs, "--env", fmt.Sprintf("GOCOVERDIR=%s", coverDir))
+	}
+
+	if len(os.Args) < 2 {
+		log.Fatalf("Not enough arguments")
+	}
+
+	action, args := os.Args[1], os.Args[2:]
+	args = append(defaultArgs, args...)
+
+	serviceFile, err := pam_test.CreateService(tmpDir, "authd-cli", []pam_test.ServiceLine{
+		{Action: pam_test.Auth, Control: pam_test.SufficientRequisite, Module: execModule, Args: args},
+		{Action: pam_test.Auth, Control: pam_test.Sufficient, Module: pam_test.Ignore.String()},
+		{Action: pam_test.Account, Control: pam_test.SufficientRequisite, Module: execModule, Args: args},
+		{Action: pam_test.Account, Control: pam_test.Sufficient, Module: pam_test.Ignore.String()},
+		{Action: pam_test.Password, Control: pam_test.SufficientRequisite, Module: execModule, Args: args},
+		{Action: pam_test.Password, Control: pam_test.Sufficient, Module: pam_test.Ignore.String()},
+	})
+	if err != nil {
+		log.Fatalf("Can't create service file %s: %v", serviceFile, err)
+	}
+
+	tx, err := pam.StartConfDir(filepath.Base(serviceFile), "", pam.ConversationFunc(
 		func(style pam.Style, msg string) (string, error) {
 			switch style {
 			case pam.TextInfo:
@@ -28,36 +82,32 @@ func main() {
 				return "", fmt.Errorf("PAM style %d not implemented", style)
 			}
 			return "", nil
-		}))
+		}), filepath.Dir(serviceFile))
+	if err != nil {
+		log.Fatalf("Impossible to start transaction %v: %v", cliPath, err)
+	}
+	defer tx.End()
 
 	var resultMsg string
-	var pamFunc pam.ModuleHandlerFunc
-	action, args := os.Args[1], os.Args[2:]
+	var pamFunc func(pam.Flags) error
 	switch action {
 	case "login":
-		pamFunc = module.Authenticate
+		pamFunc = tx.Authenticate
 		resultMsg = "PAM Authenticate() for user %q"
 	case "passwd":
-		pamFunc = module.ChangeAuthTok
+		pamFunc = tx.ChangeAuthTok
 		resultMsg = "PAM ChangeAuthTok() for user %q"
 	default:
 		panic("Unknown PAM operation: " + action)
 	}
 
-	defaultArgs := []string{"debug=true"}
-	if logDir != "" {
-		logPath := filepath.Join(logDir, "authd-pam-cli.log")
-		defaultArgs = append(defaultArgs, "logfile="+logPath)
-	}
-
-	args = append(defaultArgs, args...)
-	pamRes := pamFunc(mTx, pam.Flags(0), args)
-	user, _ := mTx.GetItem(pam.User)
+	pamRes := pamFunc(pam.Flags(0))
+	user, _ := tx.GetItem(pam.User)
 
 	printPamResult(fmt.Sprintf(resultMsg, user), pamRes)
 
 	// Simulate setting auth broker as default.
-	printPamResult("PAM AcctMgmt()", module.AcctMgmt(mTx, pam.Flags(0), args))
+	printPamResult("PAM AcctMgmt()", tx.AcctMgmt(pam.Flags(0)))
 }
 
 func printPamResult(action string, result error) {
@@ -71,4 +121,41 @@ func printPamResult(action string, result error) {
 		return
 	}
 	fmt.Printf("%s exited with success\n", action)
+}
+
+func getPkgConfigFlags(args []string) ([]string, error) {
+	out, err := exec.Command("pkg-config", args...).CombinedOutput()
+	if err != nil {
+		fmt.Errorf("can't get pkg-config dependencies: %w: %s", err, out)
+	}
+	return strings.Split(strings.TrimSpace(string(out)), " "), nil
+}
+
+func buildExecModule(path string) (string, error) {
+	execModule := filepath.Join(path, "pam_exec.so")
+	deps, err := getPkgConfigFlags([]string{"--cflags", "--libs", "gio-2.0", "gio-unix-2.0"})
+	if err != nil {
+		return "", err
+	}
+	cmd := exec.Command("cc", "pam/go-exec/module.c", "-o", execModule,
+		"-shared", "-fPIC")
+	cmd.Args = append(cmd.Args, deps...)
+	cmd.Dir = testutils.ProjectRoot()
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		return "", fmt.Errorf("can't compile exec module %s: %w\n%s", execModule, err, out)
+	}
+
+	return execModule, nil
+}
+
+func buildClient(path string) (string, error) {
+	cliPath := filepath.Join(path, "exec-client")
+	cmd := exec.Command("go", "build", "-C", "pam", "-o", cliPath)
+	cmd.Dir = testutils.ProjectRoot()
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		return "", fmt.Errorf("can't compile client %s: %v\n%s", cliPath, err, out)
+	}
+	return cliPath, nil
 }
