d5575d6636f407c250e0b78151b52b0bd3ccb61a
Marco Trevisan (Treviño) <mail@3v1n0.net>
2024-06-19 17:32:42 +0200

    pam/integration-tests: Add native PAM client integration tests
    Reusing the same test cases of the CLI client, tuned to work with the
    simpler UI that PAM provides

--- pam/integration-tests/cli_test.go
+++ pam/integration-tests/cli_test.go
@@ -28,6 +28,7 @@ func TestCLIAuthenticate(t *testing.T) {
 	currentDir, err := os.Getwd()
 	require.NoError(t, err, "Setup: Could not get current directory for the tests")
 
+	//nolint:dupl // Indeed, we want to duplicate as much of cases as we can with native UI!
 	tests := map[string]struct {
 		tape string
 
@@ -68,7 +69,7 @@ func TestCLIAuthenticate(t *testing.T) {
 
 			outDir := t.TempDir()
 
-			cliEnv := prepareCLITest(t, outDir)
+			cliEnv := prepareClientTest(t, outDir)
 			cliLog := prepareCLILogging(t)
 			t.Cleanup(func() {
 				saveArtifactsForDebug(t, []string{
@@ -121,7 +122,7 @@ func TestCLIChangeAuthTok(t *testing.T) {
 	t.Parallel()
 
 	outDir := t.TempDir()
-	cliEnv := prepareCLITest(t, outDir)
+	cliEnv := prepareClientTest(t, outDir)
 
 	// we don't care about the output of gpasswd for this test, but we still need to mock it.
 	err := os.MkdirAll(filepath.Join(outDir, "gpasswd"), 0700)
@@ -250,7 +251,7 @@ func runAuthd(t *testing.T, gpasswdOutput, groupsFile string, currentUserAsRoot 
 	return socketPath
 }
 
-func prepareCLITest(t *testing.T, clientPath string) []string {
+func prepareClientTest(t *testing.T, clientPath string) []string {
 	t.Helper()
 
 	// Due to external dependencies such as `vhs`, we can't run the tests in some environments (like LP builders), as we
