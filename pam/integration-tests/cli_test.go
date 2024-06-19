30b51563e178c72a0c11aebf5788ab557d21c12c
Marco Trevisan (Treviño) <mail@3v1n0.net>
2024-06-19 17:40:03 +0200

    pam/integration-tests: Add test checking for resetting pw on optional request
    We did check skipping this phase but not actually doing it, so let's
    test this, so that it can be later applied to the native client too.
    
    This requires adding another test user to the test broker not to clash
    with the other resetting case we have (that may lead to issues depending
    on the testing order)

--- pam/integration-tests/cli_test.go
+++ pam/integration-tests/cli_test.go
@@ -41,6 +41,7 @@ func TestCLIAuthenticate(t *testing.T) {
 		"Authenticate user with qr code":                      {tape: "qr_code"},
 		"Authenticate user and reset password":                {tape: "mandatory_password_reset"},
 		"Authenticate user and offer password reset":          {tape: "optional_password_reset_skip"},
+		"Authenticate user and accept password reset":         {tape: "optional_password_reset_do"},
 		"Authenticate user switching auth mode":               {tape: "switch_auth_mode"},
 		"Authenticate user switching username":                {tape: "switch_username"},
 		"Authenticate user switching broker":                  {tape: "switch_broker"},
