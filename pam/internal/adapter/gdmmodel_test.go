d4530f11b3ea137976251e881bf03cdf26be06f4
Marco Trevisan (Treviño) <mail@3v1n0.net>
2024-06-20 20:14:47 +0200

    gdmmodel: Fix typo

--- pam/internal/adapter/gdmmodel_test.go
+++ pam/internal/adapter/gdmmodel_test.go
@@ -605,7 +605,7 @@ func TestGdmModel(t *testing.T) {
 				gdm.EventType_startAuthentication,
 				gdm.EventType_authEvent, // retry
 				gdm.EventType_startAuthentication,
-				gdm.EventType_authEvent, // denied
+				gdm.EventType_authEvent, // granted
 			},
 			wantMessages: []tea.Msg{
 				startAuthentication{},
