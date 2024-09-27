731867dc0e3182265f0198699ad2a48508a12ae7
Marco Trevisan (Treviño) <mail@3v1n0.net>
2024-09-27 05:40:11 +0200

    tests: Add and use native detection for address sanitizer
    Instead of relying on cgo, we can just get from go if asan support is
    enabled, so rely on the build parameter instead of calling the C code

--- pam/internal/pam_test/utils.go
+++ pam/internal/pam_test/utils.go
@@ -3,21 +3,10 @@ package pam_test
 
 /*
 #include <stdlib.h>
-#include <stdbool.h>
 
 #ifdef __SANITIZE_ADDRESS__
 #include <sanitizer/lsan_interface.h>
 #endif
-
-static inline bool
-have_asan_support (void)
-{
-#ifdef __SANITIZE_ADDRESS__
-	return true;
-#else
-	return false;
-#endif
-}
 
 static inline void
 maybe_do_leak_check (void)
@@ -46,11 +35,6 @@ func MaybeDoLeakCheck() {
 	runtime.GC()
 	time.Sleep(time.Millisecond * 10)
 	C.maybe_do_leak_check()
-}
-
-// IsAddressSanitizerActive can be used to detect if address sanitizer is active.
-func IsAddressSanitizerActive() bool {
-	return bool(C.have_asan_support())
 }
 
 func allocateCBytes(bytes []byte) pam.BinaryPointer {
