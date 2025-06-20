71aa718e4c3a0d6347c601b3d57c31c3e0d6295c
Marco Trevisan (Treviño) <mail@3v1n0.net>
2025-06-20 22:15:34 +0200

    users/tempentries: Protect temporary entities and reduce the NSS lookups
    Change temporary entities public API so that all the actions that may
    involve locking the local entries are protected by a proxy struct type
    (TemporaryRecordsLocked) that can be generated by calling LockForChanges()
    that also returns a cleanup function that will unlock the structure
    again.
    
    By locking we lock the user entries database and we fetch them, so that
    they are ready to use as soon an operation would need them.
    
    This is safe since at this point no other NSS source is allowed to make
    changes to them, so they can be used until the unlock operation is not
    triggered

--- internal/users/tempentries/tempentries_test.go
+++ internal/users/tempentries/tempentries_test.go
@@ -3,14 +3,131 @@ package tempentries
 import (
 	"fmt"
 	"slices"
+	"sync"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 	"github.com/ubuntu/authd/internal/testutils/golden"
 	"github.com/ubuntu/authd/internal/users/idgenerator"
+	userslocking "github.com/ubuntu/authd/internal/users/locking"
 	"github.com/ubuntu/authd/internal/users/types"
 	"github.com/ubuntu/authd/log"
 )
+
+func TestLockedInvalidActions(t *testing.T) {
+	t.Parallel()
+
+	tmpRecords := NewTemporaryRecords(&idgenerator.IDGenerator{})
+
+	records, recordsUnlock, err := tmpRecords.LockForChanges()
+	require.NoError(t, err, "Setup: failed to lock the temporary records")
+	err = recordsUnlock()
+	require.NoError(t, err, "recordsUnlock should not fail to unlock the temporary records")
+
+	err = recordsUnlock()
+	require.Error(t, err, "Cleaning up twice should fail")
+
+	require.Panics(t, func() { _, _ = records.RegisterPreAuthUser("foobar") },
+		"RegisterPreAuthUser should panic but did not")
+	require.Panics(t, func() { _, _, _ = records.RegisterUser("foobar") },
+		"RegisterUser should panic but did not")
+	require.Panics(t, func() { _, _, _ = records.RegisterGroupForUser(123, "foobar") },
+		"RegisterGroupForUser should panic but did not")
+
+	// This is to ensure that we're in a good state, despite the actions above
+	for range 10 {
+		_, recordsUnlock, err := tmpRecords.LockForChanges()
+		require.NoError(t, err, "Failed to lock the temporary records")
+		defer func() {
+			err := recordsUnlock()
+			require.NoError(t, err, "recordsUnlock should not fail to unlock the temporary records")
+		}()
+	}
+}
+
+func TestRacingLockingActions(t *testing.T) {
+	t.Parallel()
+
+	const nIterations = 100
+
+	tmpRecords := NewTemporaryRecords(&idgenerator.IDGenerator{
+		UIDMin: 0,
+		UIDMax: nIterations * 3,
+		GIDMin: 0,
+		GIDMax: nIterations * 3,
+	})
+
+	wg := sync.WaitGroup{}
+	wg.Add(nIterations)
+
+	// Lock and get the values in parallel.
+	for idx := range nIterations {
+		t.Run(fmt.Sprintf("iteration_%d", idx), func(t *testing.T) {
+			t.Parallel()
+
+			t.Cleanup(wg.Done)
+
+			doPreAuth := idx%3 == 0
+
+			records, recordsUnlock, err := tmpRecords.LockForChanges()
+			require.NoError(t, err, "Setup: failed to lock the temporary records")
+			t.Cleanup(func() {
+				err = recordsUnlock()
+				require.NoError(t, err, "recordsUnlock should not fail to unlock the temporary records")
+			})
+
+			userName := fmt.Sprintf("authd-test-user%d", idx)
+
+			var preauthUID uint32
+			if doPreAuth {
+				// In the pre-auth case we do even more parallelization, so that
+				// the pre-auth happens without a defined order of the actual
+				// registration.
+				preauthUID, err = records.RegisterPreAuthUser(userName)
+				require.NoError(t, err, "RegisterPreAuthUser should not fail but it did")
+			}
+
+			userRegistration := func(t *testing.T) {
+				t.Helper()
+
+				uid, userCleanup, err := records.RegisterUser(userName)
+				require.NoError(t, err, "RegisterUser should not fail but it did")
+				t.Cleanup(userCleanup)
+
+				if doPreAuth {
+					require.Equal(t, preauthUID, uid, "Registered UID does not match pre-auth user")
+				}
+
+				groupName := fmt.Sprintf("authd-test-group%d", idx)
+				_, groupCleanup, err := records.RegisterGroupForUser(uid, groupName)
+				require.NoError(t, err, "RegisterGroupForUser should not fail but it did")
+				t.Cleanup(groupCleanup)
+			}
+
+			if doPreAuth {
+				t.Run("finish_registration", userRegistration)
+				return
+			}
+
+			userRegistration(t)
+		})
+	}
+
+	t.Run("final_check", func(t *testing.T) {
+		t.Parallel()
+		wg.Wait()
+
+		// Get a last cleanup function, to see if we're all good...
+		records, recordsUnlock, err := tmpRecords.LockForChanges()
+		require.NoError(t, err, "Failed to lock the temporary records")
+		err = recordsUnlock()
+		require.NoError(t, err, "recordsUnlock should not fail to unlock the users group")
+
+		// Ensure that we had cleaned up all the locks correctly!
+		require.Panics(t, func() { _, _ = records.RegisterPreAuthUser("foobar") },
+			"RegisterPreAuthUser should panic but did not")
+	})
+}
 
 func TestRegisterUser(t *testing.T) {
 	t.Parallel()
@@ -74,7 +191,7 @@ func TestRegisterUser(t *testing.T) {
 				require.NoError(t, err, "addPreAuthUser should not return an error, but did")
 			}
 
-			uid, cleanup, err := records.RegisterUser(tc.userName)
+			uid, cleanup, err := records.registerUser(tc.userName)
 			if tc.wantErr {
 				require.Error(t, err, "RegisterUser should return an error, but did not")
 				return
@@ -328,8 +445,16 @@ func TestRegisterUserAndGroupForUser(t *testing.T) {
 					replacingPreAuthUser = err == nil
 				}
 
+				records, recordsUnlock, err := records.LockForChanges()
+				require.NoError(t, err, "LockForChanges should not return an error, but did")
+				t.Cleanup(func() {
+					err := recordsUnlock()
+					require.NoError(t, err, "recordsCleanup should not return an error, but did")
+				})
+
+				internalRecords := records.tr
+
 				var uid uint32
-				var err error
 				var cleanup func()
 				if uc.preAuth {
 					uid, err = records.RegisterPreAuthUser(uc.name)
@@ -362,7 +487,7 @@ func TestRegisterUserAndGroupForUser(t *testing.T) {
 				}
 
 				require.Equal(t, uc.wantUID, uid, "%q UID is not matching expected value", uc.name)
-				require.Equal(t, wantRegisteredUsers, len(records.users),
+				require.Equal(t, wantRegisteredUsers, len(internalRecords.users),
 					"Number of pre-auth registered, users should be %d", wantRegisteredUsers)
 
 				if isDuplicated {
@@ -380,13 +505,13 @@ func TestRegisterUserAndGroupForUser(t *testing.T) {
 
 					if replacingPreAuthUser {
 						wantRegisteredUsers--
-						require.Equal(t, wantRegisteredUsers, len(records.users),
+						require.Equal(t, wantRegisteredUsers, len(internalRecords.users),
 							"Number of pre-auth registered, users should be %d", wantRegisteredUsers)
 					}
 				}
 
 				// Check that the user was registered
-				user, err := records.userByLogin(uc.name)
+				user, err := internalRecords.userByLogin(uc.name)
 				if uc.preAuth || (replacingPreAuthUser && !uc.runCleanup) {
 					require.NoError(t, err, "UserByID should not return an error, but did")
 				} else {
@@ -406,9 +531,9 @@ func TestRegisterUserAndGroupForUser(t *testing.T) {
 
 					if uc.simulateUIDRace {
 						t.Logf("Dropping the registered UID %d for %q", uid, uc.name)
-						t.Log(records.preAuthUserRecords.users)
-						delete(records.preAuthUserRecords.users, uid)
-						t.Log("after", records.preAuthUserRecords.users)
+						t.Log(internalRecords.preAuthUserRecords.users)
+						delete(internalRecords.preAuthUserRecords.users, uid)
+						t.Log("after", internalRecords.preAuthUserRecords.users)
 
 						lastCleanupIdx = idx + 1
 						registeredUIDs = slices.DeleteFunc(registeredUIDs,
@@ -418,7 +543,7 @@ func TestRegisterUserAndGroupForUser(t *testing.T) {
 
 					if uc.simulateNameRace {
 						t.Logf("Dropping the registered login name %q for %d", uc.name, uid)
-						delete(records.preAuthUserRecords.uidByLogin, uc.name)
+						delete(internalRecords.preAuthUserRecords.uidByLogin, uc.name)
 					}
 
 					// We don't have groups registration for the pre-check user.
@@ -429,14 +554,14 @@ func TestRegisterUserAndGroupForUser(t *testing.T) {
 
 				if uc.simulateUIDRace {
 					t.Logf("Dropping the registered UID %d for %q", uid, uc.name)
-					delete(records.idTracker.ids, uid)
+					delete(internalRecords.idTracker.ids, uid)
 					continue
 				}
 
 				if uc.simulateNameRace {
 					// We drop the registered name to check if the logic can handle such case.
 					t.Logf("Dropping the registered user name %q for %d", uc.name, uid)
-					delete(records.idTracker.userNames, uc.name)
+					delete(internalRecords.idTracker.userNames, uc.name)
 					lastCleanupIdx = idx + 1
 					continue
 				}
@@ -484,11 +609,11 @@ func TestRegisterUserAndGroupForUser(t *testing.T) {
 
 					require.NoError(t, err, "RegisterGroup should not return an error, but did")
 					require.Equal(t, wantGID, gid, "%q GID is not matching expected value", groupName)
-					require.Equal(t, wantRegisteredGroups, len(records.groups),
+					require.Equal(t, wantRegisteredGroups, len(internalRecords.groups),
 						"Number of groups registered, users should be %d", wantRegisteredGroups)
 
 					// Check that the temporary group was created
-					group, err := records.GroupByID(gid)
+					group, err := internalRecords.GroupByID(gid)
 					require.NoError(t, err, "GroupByID should not return an error, but did")
 
 					groupSuffix := groupName
@@ -537,7 +662,7 @@ func TestUserByIDAndName(t *testing.T) {
 			records := NewTemporaryRecords(idGeneratorMock)
 
 			if tc.registerUser {
-				uid, cleanup, err := records.RegisterUser(userName)
+				uid, cleanup, err := records.registerUser(userName)
 				require.NoError(t, err, "RegisterUser should not return an error, but did")
 				require.Equal(t, uidToGenerate, uid, "UID should be the one generated by the IDGenerator")
 				t.Cleanup(cleanup)
@@ -565,4 +690,13 @@ func checkUser(t *testing.T, user types.UserEntry, options ...golden.Option) {
 	t.Helper()
 
 	golden.CheckOrUpdateYAML(t, user, options...)
+}
+
+func TestMain(m *testing.M) {
+	log.SetLevel(log.DebugLevel)
+
+	userslocking.Z_ForTests_OverrideLocking()
+	defer userslocking.Z_ForTests_RestoreLocking()
+
+	m.Run()
 }
